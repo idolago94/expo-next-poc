177c1678c124c13052595127705f5a9dd56b3a34
Ido Lago <idolago94@gmail.com>
2024-12-13 19:58:05 +0800

    fix _app component props ts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
+import { AppProps } from "next/app";
 import Head from "next/head";
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
